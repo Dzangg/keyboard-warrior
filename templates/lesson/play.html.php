27e1df9df2adf0f251b52e7546ddd3d3c35af43b
Łukasz Kasztelan <kl51000@zut.edu.pl>
2024-01-15 22:22:11 +0100

    done

--- templates/lesson/play.html.php
+++ templates/lesson/play.html.php
@@ -9,15 +9,16 @@ $title = "Lesson {$lesson->getId()}";
 $bodyClass = 'index';
 
 ob_start(); ?>
-    <h1>Lesson <?= $lesson->getId() ?> </h1>
 
-    <h2>Difficulty:  <?= $lesson->getDifficulty() ?> </h2>
-    <h2>Learning letters:  <?= $lesson->getLetters() ?> </h2>
-    <div id="overlay" class="overlay">
-        <div id="modal" class="modal">
-            <p>Lesson <span id="result"></span> <br> Mistakes count: <span id="mistakesCount"></span> <br>Accuracy: <span id="accuracy"></span>%</p>
-            <p><a href="<?= $router->generatePath('lesson-index') ?>">Back to lessons</a></p>
-        </div>
+    <audio id="keyPressAudio">
+        <source src="/assets/media/keyb.mp3" type="audio/mpeg">
+        Your browser does not support the audio element.
+    </audio>
+
+    <div class="lesson-info">
+        <h1 class="lesson-name">Lesson <?= $lesson->getId() ?> </h1>
+        <h2 class="lesson-difficulty">Difficulty:  <?= $lesson->getDifficulty() ?> </h2>
+        <h2 class="lesson-letters">Learning letters:  <?= $lesson->getLetters() ?> </h2>
     </div>
 
 <?php
@@ -25,16 +26,105 @@ $contentArray = str_split($lesson->getContent());
 
 // Counter variable for generating unique IDs
 $counter = 1;
+echo '<div class="lesson-content">';
 
 foreach ($contentArray as $item) {
+    // Check if $item is whitespace and replace it with &nbsp;
+    $itemContent = ($item === ' ') ? '&nbsp;' : $item;
+
     $itemId = 'character' . $counter;
-    echo '<span  id="' . $itemId . '">' . $item . '</span>';
+    echo '<div class="lesson-letter-container" id="container' . $counter .'">';
+    echo '<p class="lesson-letter" id="' . $itemId . '">' . $itemContent . '</p>';
+    echo '</div>';
     $counter++;
 }
+echo '</div>';
 ?>
 
+    <div class="test">
+        <div class="keyboard">
+            <div class="row">
+                <div class="key">`</div>
+                <div class="key">1</div>
+                <div class="key">2</div>
+                <div class="key">3</div>
+                <div class="key">4</div>
+                <div class="key">5</div>
+                <div class="key">6</div>
+                <div class="key">7</div>
+                <div class="key">8</div>
+                <div class="key">9</div>
+                <div class="key">0</div>
+                <div class="key delete">DELETE</i></div>
+            </div>
+            <div class="row">
+                <div class="key tab">TAB</div>
+                <div class="key">Q</div>
+                <div class="key">W</div>
+                <div class="key">E</div>
+                <div class="key">R</div>
+                <div class="key">T</div>
+                <div class="key">Y</div>
+                <div class="key">U</div>
+                <div class="key">I</div>
+                <div class="key">O</div>
+                <div class="key">P</div>
+                <div class="key">[</div>
+                <div class="key">]</div>
+                <div class="key">\</div>
+            </div>
+            <div class="row">
+                <div class="key capslock">CAPSLOCK</div>
+                <div class="key">A</div>
+                <div class="key">S</div>
+                <div class="key">D</div>
+                <div class="key">F</div>
+                <div class="key">G</div>
+                <div class="key">H</div>
+                <div class="key">J</div>
+                <div class="key">K</div>
+                <div class="key">L</div>
+                <div class="key">;</div>
+                <div class="key">'</div>
+                <div class="key enter">ENTER</div>
+            </div>
+            <div class="row">
+                <div class="key left shift">SHIFT</div>
+                <div class="key">Z</div>
+                <div class="key">X</div>
+                <div class="key">C</div>
+                <div class="key">V</div>
+                <div class="key">B</div>
+                <div class="key">N</div>
+                <div class="key">M</div>
+                <div class="key">,</div>
+                <div class="key">.</div>
+                <div class="key">/</div>
+                <div class="key left shift">SHIFT</div>
+            </div>
+            <div class="row">
+                <div class="key ctrl">CTRL</div>
+                <div class="key alt">ALT</div>
+                <div class="key space">SPACE</div>
+                <div class="key alt">ALT</div>
+                <div class="key ctrl">CTRL</div>
+            </div>
+        </div>
+    </div>
 
-    <p><a href="<?= $router->generatePath('lesson-index') ?>">Back to lessons</a></p>
+    <div id="overlay" class="overlay">
+        <div id="modal" class="modal">
+            <p>Lesson <span id="result"></span> <br> Mistakes count: <span id="mistakesCount"></span> <br>Accuracy: <span id="accuracy"></span>%</p>
+        </div>
+    </div>
+
+    <div id="startOverlay" class="overlay start-overlay">
+        <div id="modal" class="modal">
+            Press any key to start
+        </div>
+    </div>
+
+    <p class="back-link"><a href="<?= $router->generatePath('lesson-index') ?>">Back to lessons</a></p>
 
     <script>
         class Lesson {
@@ -45,15 +135,28 @@ foreach ($contentArray as $item) {
                 this.accuracyResult = 0;
                 this.finalResultLesson = 0;
                 this.isFinished = false;
+                this.isStarted = false;
+                this.firstKeyPress = true; // Add a flag to track the first key press
                 this.initializeLesson();
             }
 
             initializeLesson() {
+                this.showStartOverlay();
                 this.applyCurrentStyle(this.currentPosition);
 
                 document.addEventListener('keydown', (event) => {
                     if (this.isFinished) {
                         return;
+                    }
+
+                    if (!this.isStarted) {
+                        this.hideStartOverlay();
+                        this.isStarted = true;
+                    }
+
+                    if (this.firstKeyPress) {
+                        this.firstKeyPress = false;
+                        return; // Ignore the first key press
                     }
 
                     const properKeyName = this.lessonContent[this.currentPosition - 1];
@@ -96,7 +199,8 @@ foreach ($contentArray as $item) {
 
             validate(isValid) {
                 const characterId = 'character' + this.currentPosition;
-                const characterElement = document.getElementById(characterId);
+                const containerName = 'container' + this.currentPosition;
+                const characterElement = document.getElementById(containerName);
 
                 if (isValid) {
                     characterElement.classList.add('valid');
@@ -126,10 +230,42 @@ foreach ($contentArray as $item) {
                 setLesson(lessonId, result);
             }
 
+            showStartOverlay() {
+                const startOverlay = document.getElementById('startOverlay');
+                startOverlay.style.display = 'flex';
+
+                // Dodaj obsługę kliknięcia na overlay tylko wtedy, gdy jest widoczny
+                startOverlay.addEventListener('click', () => {
+                    this.hideStartOverlay();
+                });
+
+                document.addEventListener('keydown', () => {
+                    this.hideStartOverlay();
+                });
+            }
+
+            hideStartOverlay() {
+                const startOverlay = document.getElementById('startOverlay');
+                startOverlay.style.display = 'none';
+            }
+
             showOverlay() {
                 var overlay = document.getElementById('overlay');
                 overlay.style.display = 'flex';
                 document.body.style.backgroundColor = 'rgba(0, 0, 0, 0.2)';
+
+                // Dodaj obsługę kliknięcia na tło overlay tylko wtedy, gdy jest widoczne
+                overlay.addEventListener('click', (event) => {
+                    if (event.target === document.getElementById('overlay')) {
+                        this.hideOverlay();
+                    }
+                });
+            }
+
+            hideOverlay() {
+                var overlay = document.getElementById('overlay');
+                overlay.style.display = 'none';
+                document.body.style.backgroundColor = ''; // Przywróć kolor tła
             }
 
             mistakesQuantity() {
@@ -153,6 +289,27 @@ foreach ($contentArray as $item) {
 
         // Create an instance of the Lesson class with the lesson content
         const lessonInstance = new Lesson(<?= json_encode($lesson->getContent()) ?>);
+
+        // keyboard handler
+        let keyboard = document.getElementsByClassName('keyboard')[0];
+        let keys = keyboard.getElementsByClassName('key');
+
+        document.addEventListener('keydown', (event) => {
+            for (let i = 0; i < keys.length; i++) {
+                if (event.key.toLowerCase() === keys[i].textContent.toLowerCase()) {
+                    keys[i].style.backgroundColor = 'red';
+                    keys[i].style.transition = 'background-color 0.3s ease';
+
+                    // Odtwarzaj dźwięk za pomocą elementu audio
+                    document.getElementById('keyPressAudio').play();
+                    console.log('cos')
+                    document.addEventListener('keyup', (event) => {
+                        keys[i].style.backgroundColor = '';
+                        keys[i].style.transition = 'background-color 0.9s ease';
+                    });
+                }
+            }
+        });
     </script>
 
 <?php $main = ob_get_clean();
